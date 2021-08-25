adb44860a72cade7a784b9f2582dede6044f4449
Adam Cvija <adam_sez_hi@msn.com>
2021-08-25 17:22:32 +0800

    json file creation

--- ICT397-GameEngine/LevelLoader.cpp
+++ ICT397-GameEngine/LevelLoader.cpp
@@ -1,9 +1,14 @@
 #include "LevelLoader.h"
 #include "GameObjectFactory.h"
+#include <nlohmann/json.hpp>
+#include <filesystem>
 
 #if _DEBUG
 #include <iostream>
 #endif
+
+using json = nlohmann::json;
+namespace fs = std::filesystem;
 
 LevelLoader::LevelLoader() 
 {
@@ -12,6 +17,10 @@ LevelLoader::LevelLoader()
 
 void LevelLoader::Test() 
 {
+
+	json j;
+
+	std::cout << j << std::endl;
 
 	std::map<std::string, GameObject*>::iterator it;
 
@@ -26,8 +35,41 @@ void LevelLoader::Test()
 			"	 Is at position z=" << it->second->GetTransform()->GetPosition().GetZ() << "\n" <<
 			": "<< std::endl;
 
+
+
 		i++;
 	}
+
+	
+
+
+	/// FIle pathing and tests
+	
+	fs::path myPath = fs::current_path();
+
+	fs::path newPath = (myPath /= "../Assets//SaveFiles");
+
+	std::cout << newPath << std::endl;
+
+	if (fs::create_directories(newPath)) 
+	{
+		std::cout << "SUCCESS" << std::endl;
+	}
+		
+	if (!fs::is_directory("../Assets//SaveFiles") || !fs::exists("../Assets//SaveFiles"))
+	{
+		std::cout << "File doesn't exist" << std::endl;
+	}
+
+	//////end file pathing
+
+	///now we need to generate teh JSON
+
+	
+
+	//we need to make this easier
+	std::ofstream o("../Assets/SaveFiles/tavern.json");
+	o << std::setw(4) << j << std::endl;
 	
 
 	
--- ICT397-GameEngine/LevelLoader.h
+++ ICT397-GameEngine/LevelLoader.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <map>
 #include "GameObject.h"
+
 class LevelLoader
 {
 	//Member variables
