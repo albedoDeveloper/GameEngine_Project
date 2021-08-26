27a4d64f315482c258d64dbd6237b8f83789d632
Kye <50266459+albedoDeveloper@users.noreply.github.com>
2021-08-26 16:55:06 +0800

    Improvements to Rotation, still broken somewhat

--- ICT397-GameEngine/CAABBCollider.cpp
+++ ICT397-GameEngine/CAABBCollider.cpp
@@ -14,9 +14,8 @@ CAABBCollider::CAABBCollider(Transform* parent, GameObject* parentObj)
 
 	tempVec = reactphysics3d::Vector3(parent->GetPosition().GetX(), parent->GetPosition().GetY(), parent->GetPosition().GetZ());
 	tempQuat = reactphysics3d::Quaternion(parent->GetRotation().GetX(), parent->GetRotation().GetY(), parent->GetRotation().GetZ(), parent->GetRotation().GetW());
-
-
-
+	
+	tempQuat = tempQuat.getInverse();
 
 }
 
@@ -78,9 +77,11 @@ void CAABBCollider::UpdateCollider(const Transform& transform)
 {
 	reactphysics3d::Vector3 tempVec = reactphysics3d::Vector3(transform.GetPosition().GetX(), transform.GetPosition().GetY(), transform.GetPosition().GetZ());
 	reactphysics3d::Quaternion tempQuat = reactphysics3d::Quaternion(transform.GetRotation().GetX(), transform.GetRotation().GetY(), transform.GetRotation().GetZ(), transform.GetRotation().GetW());
+	tempQuat = tempQuat.getInverse();
 
 	reactphysics3d::Transform tempTransform(tempVec, tempQuat);
 
 	colBody->setTransform(tempTransform);
+	
 
 }
