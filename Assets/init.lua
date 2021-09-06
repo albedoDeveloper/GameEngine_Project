121953f564073ccbe55021ea96772bd16717b901
Robert <robert_val1@hotmail.com>
2021-09-06 17:19:37 +0800

    floor added

--- Assets/init.lua
+++ Assets/init.lua
@@ -2,6 +2,7 @@
 LoadModel("tavern_glass_door_wall", "tavern_glass_door_wall/tavern_glass_door_wall.fbx")
 LoadModel("tavern_glass_door_wall_long", "tavern_glass_door_wall_long/tavern_glass_door_wall_long.fbx")
 LoadModel("graffiti_block", "graffiti_block/graffiti_block.fbx")
+LoadModel("tavern_floor", "tavern_floor/tavern_floor.fbx")
 LoadModel("south_bar_barrier", "south_bar_barrier/south_bar_barrier.fbx")
 LoadModel("axis", "axis/axis.fbx")
 LoadScript("rotate", "rotate.lua")
@@ -21,5 +22,8 @@ GetGameObject("graffiti_block"):AddCStaticMesh():AssignModel("graffiti_block")
 
 SpawnGameObject("south_bar_barrier")
 GetGameObject("south_bar_barrier"):AddCAABBCollider()
-GetGameObject("south_bar_barrier"):GetTransform():Translate(0,0,20)
 GetGameObject("south_bar_barrier"):AddCStaticMesh():AssignModel("south_bar_barrier")
+
+SpawnGameObject("tavern_floor")
+GetGameObject("tavern_floor"):AddCAABBCollider()
+GetGameObject("tavern_floor"):AddCStaticMesh():AssignModel("tavern_floor")
