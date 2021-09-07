e553ff8a1329253b703ecd9823361deb2cf0f5d5
Robert <robert_val1@hotmail.com>
2021-09-07 08:08:21 +0800

    toilet_block

--- Assets/init.lua
+++ Assets/init.lua
@@ -7,32 +7,38 @@ LoadModel("south_bar_barrier", "south_bar_barrier/south_bar_barrier.fbx")
 LoadModel("main_bar_bench", "main_bar_bench/main_bar_bench.fbx")
 LoadModel("bar_floor_rod", "bar_floor_rod/bar_floor_rod.fbx")
 LoadModel("axis", "axis/axis.fbx")
+LoadModel("goblin", "goblin/goblin.fbx")
+
 LoadScript("rotate", "rotate.lua")
+
+SpawnGameObject("goblin1")
+GetGameObject("goblin1"):AddCStaticMesh():AssignModel("goblin")
+GetGameObject("goblin1"):AddCCollider():AddBoxCollider(0,0,0,0,0,0,true)
 
 SpawnGameObject("tavern_glass_door_wall")
 GetGameObject("tavern_glass_door_wall"):AddCStaticMesh():AssignModel("tavern_glass_door_wall")
-GetGameObject("tavern_glass_door_wall"):AddCCollider():AddBoxCollider(0,0,0,true)
+GetGameObject("tavern_glass_door_wall"):AddCCollider():AddBoxCollider(0,0,0,0,0,0,true)
 
 SpawnGameObject("tavern_glass_door_wall_long")
 GetGameObject("tavern_glass_door_wall_long"):AddCStaticMesh():AssignModel("tavern_glass_door_wall_long")
-GetGameObject("tavern_glass_door_wall_long"):AddCCollider():AddBoxCollider(0,0,0,true)
+GetGameObject("tavern_glass_door_wall_long"):AddCCollider():AddBoxCollider(0,0,0,0,0,0,true)
 
 SpawnGameObject("graffiti_block")
 GetGameObject("graffiti_block"):AddCStaticMesh():AssignModel("graffiti_block")
-GetGameObject("graffiti_block"):AddCCollider():AddBoxCollider(0,0,0,true)
+GetGameObject("graffiti_block"):AddCCollider():AddBoxCollider(0,0,0,0,0,0,true)
 
 SpawnGameObject("south_bar_barrier")
 GetGameObject("south_bar_barrier"):AddCStaticMesh():AssignModel("south_bar_barrier")
-GetGameObject("south_bar_barrier"):AddCCollider():AddBoxCollider(0,0,0,true)
+GetGameObject("south_bar_barrier"):AddCCollider():AddBoxCollider(0,0,0,0,0,0,true)
 
 SpawnGameObject("tavern_floor")
 GetGameObject("tavern_floor"):AddCStaticMesh():AssignModel("tavern_floor")
-GetGameObject("tavern_floor"):AddCCollider():AddBoxCollider(0,0,0,true)
+GetGameObject("tavern_floor"):AddCCollider():AddBoxCollider(0,0,0,0,0,0,true)
 
 SpawnGameObject("main_bar_bench")
 GetGameObject("main_bar_bench"):AddCStaticMesh():AssignModel("main_bar_bench")
-GetGameObject("main_bar_bench"):AddCCollider():AddBoxCollider(0,0,0,true)
+GetGameObject("main_bar_bench"):AddCCollider():AddBoxCollider(0,0,0,0,0,0,true)
 
 SpawnGameObject("bar_floor_rod")
 GetGameObject("bar_floor_rod"):AddCStaticMesh():AssignModel("bar_floor_rod")
-GetGameObject("bar_floor_rod"):AddCCollider():AddBoxCollider(0,0,0,true)
+GetGameObject("bar_floor_rod"):AddCCollider():AddBoxCollider(0,0,0,0,0,0,true)
