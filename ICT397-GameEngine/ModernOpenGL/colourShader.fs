412224eadce03cf3191472313d92c0d324f4ba51
Robert <38125987+robertvalentic@users.noreply.github.com>
2021-09-09 13:25:22 +0800

    attenuation update

--- ICT397-GameEngine/ModernOpenGL/colourShader.fs
+++ ICT397-GameEngine/ModernOpenGL/colourShader.fs
@@ -24,14 +24,14 @@ void main()
     vec3 lightDir = normalize(lightPos1 - FragPos);
     float diff = max(dot(Normal, lightDir), 0.0);
     float distance = length(lightPos1 - FragPos);
-    float attenuation = 1.0 / (1 + 0.07 * distance + 0.017 * (distance * distance));
+    float attenuation = 1.0 / (1 + 0.014 * distance + 0.0007 * (distance * distance));
     diff = max(dot(Normal, lightDir), 0.0) * attenuation;
     result += lightColor1 * diff * vec3(texture(texture_diffuse1, TexCoords));
 
     lightDir = normalize(lightPos2 - FragPos);
     diff = max(dot(Normal, lightDir), 0.0);
     distance = length(lightPos2 - FragPos);
-    attenuation = 1.0 / (1 + 0.07 * distance + 0.017 * (distance * distance));
+    attenuation = 1.0 / (1 + 0.014 * distance + 0.0007 * (distance * distance));
     diff = max(dot(Normal, lightDir), 0.0) * attenuation;
     result += lightColor2 * diff * vec3(texture(texture_diffuse1, TexCoords));
 
