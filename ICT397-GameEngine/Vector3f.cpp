929dde75ba43018155ee4f692df0fd7187e17603
Robert <robert_val1@hotmail.com>
2021-09-18 18:29:29 +0800

    vector3f doxy

---
+++ ICT397-GameEngine/Vector3f.cpp
@@ -0,0 +1,130 @@
+#include "Vector3f.h"
+
+Vector3f::Vector3f()
+	:m_vec{ 0.f,0.f,0.f }
+{}
+
+Vector3f::Vector3f(float x, float y, float z)
+	: m_vec(x, y, z)
+{}
+
+void Vector3f::Translate(const Vector3f &v)
+{
+	m_vec += glm::vec3(v.GetX(), v.GetY(), v.GetZ());
+}
+
+float *Vector3f::ValuePtr()
+{
+	return glm::value_ptr(m_vec);
+}
+
+void Vector3f::Scale(float x, float y, float z)
+{
+	m_vec.x *= x;
+	m_vec.y *= y;
+	m_vec.z *= z;
+}
+
+float Vector3f::GetX() const
+{
+	return m_vec.x;
+}
+
+float Vector3f::GetY() const
+{
+	return m_vec.y;
+}
+
+void Vector3f::SetX(float x)
+{
+	m_vec.x = x;
+}
+
+void Vector3f::SetY(float y)
+{
+	m_vec.y = y;
+}
+
+void Vector3f::SetZ(float z)
+{
+	m_vec.z = z;
+}
+
+float Vector3f::Magnitude() const
+{
+	return glm::length(m_vec);
+}
+
+void Vector3f::SetMagnitude(float length)
+{
+	m_vec = (length / glm::length(m_vec)) * m_vec;
+}
+
+Vector3f Vector3f::crossProduct(Vector3f oVec)
+{
+	return glm::cross(m_vec, oVec.m_vec);
+}
+
+float Vector3f::dotProduct(Vector3f oVec)
+{
+	return glm::dot(m_vec, oVec.m_vec);
+}
+
+Vector3f Vector3f::Normalise(Vector3f oVec)
+{
+	return glm::normalize(oVec.m_vec);
+}
+
+float Vector3f::Rad2Deg(float rad)
+{
+	return rad * 180 / glm::pi<float>();
+}
+
+Vector3f Vector3f::Rad2Deg()
+{
+	Vector3f temp = m_vec;
+	Vector3f newVec;
+	newVec.SetX(Rad2Deg(temp.GetX()));
+	newVec.SetY(Rad2Deg(temp.GetY()));
+	newVec.SetZ(Rad2Deg(temp.GetZ()));
+	return newVec;
+}
+
+Vector3f Vector3f::operator+(Vector3f oVec)
+{
+	return m_vec + oVec.m_vec;
+}
+
+Vector3f Vector3f::operator*(const Vector3f &oVec)
+{
+	return m_vec * oVec.m_vec;
+}
+
+Vector3f Vector3f::operator*(const float &num)
+{
+	return glm::vec3(m_vec.x * num, m_vec.y * num, m_vec.z * num);
+}
+
+Vector3f Vector3f::operator/(const Vector3f &oVec)
+{
+	return m_vec / oVec.m_vec;
+}
+
+Vector3f Vector3f::operator-(const Vector3f &oVec)
+{
+	return m_vec - oVec.m_vec;
+}
+
+Vector3f Vector3f::operator=(const Vector3f &oVec)
+{
+	return m_vec = oVec.m_vec;
+}
+
+Vector3f Vector3f::operator*(const Quaternion &quat)
+{
+	return m_vec * quat.m_quat;
+}
+
+Vector3f::Vector3f(glm::vec3 v)
+	:m_vec(v)
+{}
--- ICT397-GameEngine/Vector3f.h
+++ ICT397-GameEngine/Vector3f.h
@@ -12,37 +12,31 @@ class Vector3f
 {
 public:
 		/**
-		* @brief Default constructor
+		* @brief Default constructor. makes a 0,0,0 vector
 		*/
-	Vector3f()
-		:m_vec(1, 1, 1)
-	{}
+	Vector3f();
 
-		/// <summary>
-		/// Non-default Constructor
-		/// </summary>
-		/// <param name="x"></param>
-		/// <param name="y"></param>
-		/// <param name="z"></param>
-	Vector3f(float x, float y, float z)
-		:m_vec(x, y, z)
-	{
-
-	}
+		/**
+		 * constructor that sets vec values
+		 *
+		 * \param x
+		 * \param y
+		 * \param z
+		 */
+	Vector3f(float x, float y, float z);
 
 		/**
 		* @brief Moves the vector by a relative amount
 		* @param v The vector to add to this vector
 		*/
-	void Translate(const Vector3f &v)
-	{
-		m_vec += glm::vec3(v.GetX(), v.GetY(), v.GetZ());
-	}
+	void Translate(const Vector3f &v);
 
-	float *ValuePtr()
-	{
-		return glm::value_ptr(m_vec);
-	}
+		/**
+		 * .
+		 *
+		 * \return
+		 */
+	float *ValuePtr();
 
 		/**
 		* @brief Scales the vector by a relative amount
@@ -50,30 +44,19 @@ public:
 		* @param y Y component of the scaling factor
 		* @param z Z component of the scaling factor
 		*/
-	void Scale(float x, float y, float z)
-	{
-		m_vec.x *= x;
-		m_vec.y *= y;
-		m_vec.z *= z;
-	}
+	void Scale(float x, float y, float z);
 
 		/**
 		 * @brief x value accessor
 		 * @return the value of X
 		*/
-	float GetX() const
-	{
-		return m_vec.x;
-	}
+	float GetX() const;
 
 		/**
 		 * @brief y value accessor
 		 * @return the value of Y
 		*/
-	float GetY() const
-	{
-		return m_vec.y;
-	}
+	float GetY() const;
 
 		/**
 		 * @brief z value accessor
@@ -88,143 +71,90 @@ public:
 		 * @brief x value mutator
 		 * @param x the desired value of X
 		*/
-	void SetX(float x)
-	{
-		m_vec.x = x;
-	}
+	void SetX(float x);
 
 		/**
 		 * @brief y value mutator
 		 * @param y the desired value of Y
 		*/
-	void SetY(float y)
-	{
-		m_vec.y = y;
-	}
+	void SetY(float y);
 
 		/**
 		 * @brief z value mutator
 		 * @param z the desired value of Z
 		*/
-	void SetZ(float z)
-	{
-		m_vec.z = z;
-	}
+	void SetZ(float z);
 
-	float Magnitude() const
-	{
-		return glm::length(m_vec);
-	}
+	float Magnitude() const;
 
-	void SetMagnitude(float length)
-	{
-		m_vec = (length / glm::length(m_vec)) * m_vec;
-	}
+	void SetMagnitude(float length);
 
 		/// <summary>
 		/// Cross product of two vectors
 		/// </summary>
 		/// <param name="oVec"></param>
 		/// <returns></returns>
-	Vector3f crossProduct(Vector3f oVec)
-	{
-		return glm::cross(m_vec, oVec.m_vec);
-	}
+	Vector3f crossProduct(Vector3f oVec);
 
 		/// <summary>
 		/// Dot Product of two vectors
 		/// </summary>
 		/// <param name="oVec"></param>
 		/// <returns></returns>
-	float dotProduct(Vector3f oVec)
-	{
-		return glm::dot(m_vec, oVec.m_vec);
-	}
+	float dotProduct(Vector3f oVec);
 
 		/// <summary>
 		/// Normalise a vector
 		/// </summary>
 		/// <param name="oVec"></param>
 		/// <returns></returns>
-	Vector3f Normalise(Vector3f oVec)
-	{
-		return glm::normalize(oVec.m_vec);
-	}
+	Vector3f Normalise(Vector3f oVec);
 
-	float Rad2Deg(float rad)
-	{
-		return rad * 180 / glm::pi<float>();
-	}
+	float Rad2Deg(float rad);
 
-	Vector3f Rad2Deg()
-	{
-		Vector3f temp = m_vec;
-		Vector3f newVec;
-		newVec.SetX(Rad2Deg(temp.GetX()));
-		newVec.SetY(Rad2Deg(temp.GetY()));
-		newVec.SetZ(Rad2Deg(temp.GetZ()));
-		return newVec;
-	}
+	Vector3f Rad2Deg();
 
 		/// <summary>
 		/// Operator + overload
 		/// </summary>
 		/// <param name="oVec"></param>
 		/// <returns></returns>
-	Vector3f operator+(Vector3f oVec)
-	{
-		return m_vec + oVec.m_vec;
-	}
+	Vector3f operator+(Vector3f oVec);
 
 		/// <summary>
 		/// Operator * overload
 		/// </summary>
 		/// <param name="oVec"></param>
 		/// <returns></returns>
-	Vector3f operator*(const Vector3f &oVec)
-	{
-		return m_vec * oVec.m_vec;
-	}
+	Vector3f operator*(const Vector3f &oVec);
 
 		/// <summary>
 		/// Operator * overload
 		/// </summary>
 		/// <param name="num"></param>
 		/// <returns></returns>
-	Vector3f operator*(const float &num)
-	{
-		return glm::vec3(m_vec.x * num, m_vec.y * num, m_vec.z * num);
-	}
+	Vector3f operator*(const float &num);
 
 		/**
 		* @brief division operator
 		* @param oVec the vector to divide by
 		* @return result of division
 		*/
-	Vector3f operator/(const Vector3f &oVec)
-	{
-		return m_vec / oVec.m_vec;
-	}
+	Vector3f operator/(const Vector3f &oVec);
 
 		/// <summary>
 		/// Operator - overload
 		/// </summary>
 		/// <param name="oVec"></param>
 		/// <returns></returns>
-	Vector3f operator-(const Vector3f &oVec)
-	{
-		return m_vec - oVec.m_vec;
-	}
+	Vector3f operator-(const Vector3f &oVec);
 
 		/**
 		* @brief assignment operator
 		* @param oVec the vector to set this vector to
 		* @return the value of the vector
 		*/
-	Vector3f operator=(const Vector3f &oVec)
-	{
-		return m_vec = oVec.m_vec;
-	}
+	Vector3f operator=(const Vector3f &oVec);
 
 		/**
 		* .
@@ -232,10 +162,7 @@ public:
 		* \param quat
 		* \return
 		*/
-	Vector3f operator*(const Quaternion &quat)
-	{
-		return m_vec * quat.m_quat;
-	}
+	Vector3f operator*(const Quaternion &quat);
 
 private:
 		/// <summary>
@@ -247,9 +174,8 @@ private:
 		/// Copy Constructor
 		/// </summary>
 		/// <param name="v"></param>
-	Vector3f(glm::vec3 v)
-		:m_vec(v)
-	{}
+	Vector3f(glm::vec3 v);
 
+		// friendship decleration
 	friend Matrix4f LookAt(const Vector3f &eye, const Vector3f &centre, const Vector3f &up);
 };
