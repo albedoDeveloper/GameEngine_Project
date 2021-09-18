87e61686a3f0734f7e99f99b798338cdc13ed51a
Adam Cvija <adam_sez_hi@msn.com>
2021-09-18 18:18:44 +0800

    collision comments

--- ICT397-GameEngine/CollisionManager.h
+++ ICT397-GameEngine/CollisionManager.h
@@ -1,3 +1,10 @@
+/*****************************************************************//**
+ * \file   CollisionManager.h
+ * \brief	Manages Collisions using React physics
+ *
+ * \date   September 2021
+ *********************************************************************/
+
 #pragma once
 
 #include "CCollider.h"
@@ -10,59 +17,60 @@
 class CollisionManager : reactphysics3d::CollisionCallback
 {
 private:
-	/**
-	 * @brief The beginning size, before alteration, of the collider awway
-	*/
+		/** @brief The beginning size, before alteration, of the collider array */
 	const static int m_initialSize = 50;
 
-	/**
-	 * @brief The current index to which the collider array is filled
-	*/
+		/** @brief The current index to which the collider array is filled */
 	int m_fill = 0;
 
-	/**
-	 * @brief Vector containing all of the colliders in the game
-	*/
+		/** @brief Vector containing all of the colliders in the game */
 	std::vector<CCollider *> m_colliderArray;
 
+		/** @brief is colliding or not */
 	bool m_collision;
 
 public:
-	/*
-@brief Physics World that allows collisions to occur between collision bodies
-*/
+		/** @brief Physics World that allows collisions to occur between collision bodies */
 	reactphysics3d::PhysicsWorld *physicsWorld;
 
+		/** @brief Common Physics world */
 	reactphysics3d::PhysicsCommon physicsCommon;
 
+		/** @brief Allows rendering of collision boxes in debug mode */
 	reactphysics3d::DebugRenderer *debugRender;
 
+		/** @brief wait tiem for collisions */
 	int waitTime = 0;
 
 public:
-	/**
-	 * @brief default constructor
-	*/
+		/**
+		 * @brief default constructor
+		*/
 	CollisionManager();
 
 	void Init();
 
-	/**
-	 * @brief Singleton instance accessor
-	 * @return the single instance of this manager
-	*/
+		/**
+		 * @brief Singleton instance accessor
+		 * @return the single instance of this manager
+		*/
 	static CollisionManager *Instance();
 
-	/**
-	 * @brief check if a AABB is colliding with any other AABB's
-	*/
+		/**
+		 * @brief check if a AABB is colliding with any other AABB's
+		*/
 	bool CheckCollision(CCollider &myCollider);
 
+		/**
+		 * @brief Override for React::CollisionCallBack onContact
+		 *
+		 * /param callbackData the data for the Callback
+		 */
 	void onContact(const CallbackData &callbackData);
 
-	/**
-	 * @Get the static collision world, to either add collisonbody or test for colliders
-	*/
+		/**
+		 * @Get the static collision world, to either add collisonbody or test for colliders
+		*/
 	reactphysics3d::PhysicsWorld *GetPhysicsWorld()
 	{
 		return physicsWorld;
