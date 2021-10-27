3beb2bbb4c99ababf8bab2e8b88a7d309e6cca32
Robert <robert_val1@hotmail.com>
2021-10-28 00:27:28 +0800

    raycast func

--- ENGINE/CollisionManager.cpp
+++ ENGINE/CollisionManager.cpp
@@ -112,6 +112,31 @@ void CollisionManager::RemoveColliderToWorld(CCollider &c)
 	m_collisionWorld.removeCollisionObject(c.m_colObj);
 }
 
+CCollider *CollisionManager::Raycast(const Vector3f &from, const Vector3f &to)
+{
+	m_colObjCache = nullptr;
+	m_collisionWorld.rayTest(
+		btVector3(from.GetX(), from.GetY(), from.GetZ()),
+		btVector3(to.GetX(), to.GetY(), to.GetZ()),
+		*COLLISION
+	);
+	if (m_colObjCache)
+	{
+		return COLLISION->GetCColliderRegistration(m_colObjCache);
+	}
+	else
+	{
+		return nullptr;
+	}
+}
+
+btScalar CollisionManager::addSingleResult(btCollisionWorld::LocalRayResult &rayResult, bool normalInWorldSpace)
+{
+	m_collision = true;
+	m_colObjCache = rayResult.m_collisionObject;
+	return btScalar();
+}
+
 void CollisionManager::FillManifoldAB(unsigned manifoldIndex)
 {
 	m_contactCache.push_back(Manifold(
