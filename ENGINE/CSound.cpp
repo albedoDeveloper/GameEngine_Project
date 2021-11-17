a2d6ddd8a4018122ca018c5a4e48307abe501c81
Kye <50266459+albedoDeveloper@users.noreply.github.com>
2021-11-17 12:05:22 +0800

    Update CSound.cpp

--- ENGINE/CSound.cpp
+++ ENGINE/CSound.cpp
@@ -28,13 +28,17 @@ void CSound::PlaySound(std::string soundName, int length, bool positional)
 
 void CSound::ChangeVolume(std::string soundName, int volume)
 {
-	Mix_Volume(soundinfo[soundName].channel, volume);
+	if (soundinfo.count(soundName) > 0)
+		Mix_Volume(soundinfo[soundName].channel, volume);
 }
 
 void CSound::StopPlaying(std::string soundName)
 {
-	Mix_HaltChannel(soundinfo[soundName].channel);
-	soundinfo.erase(soundName);
+	if (soundinfo.count(soundName) > 0)
+	{
+		Mix_HaltChannel(soundinfo[soundName].channel);
+		soundinfo.erase(soundName);
+	}
 }
 
 
